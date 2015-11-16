355cbc3aacd856f47d1f2e7de6dbfe214e81ecd0
Michael Koch <tensberg@gmx.net>
2015-11-16 21:45:52 +0100

    initial project setup

---
+++ README.md
@@ -0,0 +1,9 @@
+JGloss Live
+===========
+
+Web-based version of JGloss.
+
+Toy/Demo application. Proof of concept for various technologies.
+
+(C) 2015 Michael Koch <tensberg@gmx.net>
+
---
+++ src/main/java/net/sf/jgloss/live/JGlossLive.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2002-2015 Michael Koch (tensberg@gmx.net)
+ *
+ * This file is part of JGloss.
+ *
+ * JGloss is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * JGloss is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JGloss; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package net.sf.jgloss.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class JGlossLive {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(JGlossLive.class, args);
+    }
+}
